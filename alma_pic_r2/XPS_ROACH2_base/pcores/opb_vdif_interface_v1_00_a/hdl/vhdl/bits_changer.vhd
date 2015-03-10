931febf545e94acec24cee7e0849b9e6c18323af
Rich Lacasse <rlacasse@nrao.edu>
2015-03-10 08:46:49 -0400

    Version 5; Used for Jan15, 2015 system tests revised bit mapping to be compatible with Haystack correlator revised to include mapping for simulation as well as an Id string for CVS changed Oct. 10, 2014

--- alma_pic_r2/XPS_ROACH2_base/pcores/opb_vdif_interface_v1_00_a/hdl/vhdl/bits_changer.vhd
+++ alma_pic_r2/XPS_ROACH2_base/pcores/opb_vdif_interface_v1_00_a/hdl/vhdl/bits_changer.vhd
@@ -5,6 +5,8 @@ use IEEE.NUMERIC_STD.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 -------------------------------------------------------
 
+-- # $Id: bits_changer.vhd,v 1.3 2014/10/10 18:33:48 rlacasse Exp $
+
 
 entity bits_changer is
   port(
@@ -22,10 +24,17 @@ signal output_sig : std_logic_vector(1 downto 0):= "00";
          begin
 		 if(rising_edge(clk)) then
 			case I is                 -- data selection signals
-	      	  		when "01"  => output_sig <= "11";	-- +3
-				when "00"  => output_sig <= "10";     	-- +1
+--      mapping for simulation			
+--	     	when "01"  => output_sig <= "01";	-- +3
+--				when "00"  => output_sig <= "00"; -- +1
+--				when "11"  => output_sig <= "11";	-- -1
+--				when "10"  => output_sig <= "10";	-- -3
+--      normal mapping				
+	     	when "01"  => output_sig <= "11";	-- +3
+				when "00"  => output_sig <= "10"; -- +1
 				when "11"  => output_sig <= "01";	-- -1
 				when "10"  => output_sig <= "00";	-- -3
+								
 				when others => output_sig <= "00";
 	       		end case;
 		end if;
